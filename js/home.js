aacd63b9e8270d4d728fb4ea38062b80ea68d77e
FireCartagena <60412518+FireCartagena@users.noreply.github.com>
2024-11-12 22:58:14 -0600

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    const dataUsuario = infoUser();
+
+    // Definimos los elementos de la pantalla a cambiar
+    const usuarioNombre = document.getElementById("usuarioNombre");
+    const usuarioCuenta = document.getElementById("usuarioCuenta");
+    const usuarioSaldo = document.getElementById("usuarioSaldo");
+
+    // Cambiamos la informacion principal
+    usuarioNombre.innerHTML = dataUsuario.nombre;
+    usuarioCuenta.innerHTML = dataUsuario.cuenta;
+    usuarioSaldo.innerHTML = dataUsuario.saldo.toFixed(2);
+
+    /**
+     * Funcion para interceptar la accion del boton
+     */
+    document.getElementById("btnSalir").addEventListener("click", function() {
+        salir();
+    });
+});
